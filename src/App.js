5a43b4b51add3dddc5cd053a9bd4b4c980f27b6b
mmartins <mangierre@gmail.com>
2020-06-11 19:29:42 -0300

    finished dark mode

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ const API_FORMAT_URL = 'https://sqlformat.org/api/v1/format';
 const CSS_BODY = 'body-';
 const CSS_LEGEND = 'legend-';
 const CSS_INPUT = 'input-';
+const CSS_BUTTON_LOAD = 'buttonload-';
+const CSS_RESULT = 'formatSql-';
 
 /*global chrome*/
 
@@ -40,6 +42,8 @@ class App extends React.Component {
         const bodyClass = CSS_BODY + theme;
         const legendClass = CSS_LEGEND + theme;
         const inputClass = CSS_INPUT + theme;
+        const buttonLoadClass = CSS_BUTTON_LOAD + theme;
+        const resultClass = CSS_RESULT + theme;
 
         let  body = document.querySelector('body');
         body.classList.add(bodyClass);
@@ -49,11 +53,16 @@ class App extends React.Component {
             legend[i].classList.add(legendClass);
         }
 
-        let input = document.querySelector('input');
-        input.classList.add(inputClass);
+        let e = document.querySelector('input');
+        e.classList.add(inputClass);
 
-        console.log(legend);
+        e = document.querySelectorAll('.buttonload');
+        for (let i = 0; i < legend.length; i++) {
+            e[i].classList.add(buttonLoadClass);
+        }
 
+        e = document.querySelector('#formatSql');
+        e.classList.add(resultClass);
     }
 
     handleChange(event) {
@@ -75,244 +84,239 @@ class App extends React.Component {
         formData.append("reindent", 1);
         formData.append("sql", this.state.query);
 
-        fetch(API_FORMAT_URL, {
-            method: 'POST',
-            body: formData
-        }).then(res => res.json()).then((result) => {
-            let replace = function(data) {
+        let data = 'SELECT * from sales_order';
+        let replace = function(data) {
 
-                var reserved = [
-                    "add ",
-                    "all ",
-                    "alter ",
-                    "analyze ",
-                    "and ",
-                    "as ",
-                    "asc ",
-                    "asensitive ",
-                    "before ",
-                    "between ",
-                    "bigint ",
-                    "binary ",
-                    "blob ",
-                    "both ",
-                    "by ",
-                    "call ",
-                    "cascade ",
-                    "case ",
-                    "change ",
-                    "char ",
-                    "character ",
-                    "check ",
-                    "collate ",
-                    "column ",
-                    "condition ",
-                    "connection ",
-                    "constraint ",
-                    "continue ",
-                    "convert ",
-                    "create ",
-                    "cross ",
-                    "current_date ",
-                    "current_time ",
-                    "current_timestamp ",
-                    "current_user ",
-                    "cursor ",
-                    "database ",
-                    "databases ",
-                    "day_hour ",
-                    "day_microsecond ",
-                    "day_minute ",
-                    "day_second ",
-                    "dec ",
-                    "decimal ",
-                    "declare ",
-                    "default ",
-                    "delayed ",
-                    "delete ",
-                    "desc ",
-                    "describe ",
-                    "deterministic ",
-                    "distinct ",
-                    "distinctrow ",
-                    "div ",
-                    "double ",
-                    "drop ",
-                    "dual ",
-                    "each ",
-                    "else ",
-                    "elseif ",
-                    "enclosed ",
-                    "escaped ",
-                    "exists ",
-                    "exit ",
-                    "explain ",
-                    "false ",
-                    "fetch ",
-                    "float ",
-                    "for ",
-                    "force ",
-                    "foreign ",
-                    "from ",
-                    "fulltext ",
-                    "goto ",
-                    "grant ",
-                    "group ",
-                    "having ",
-                    "high_priority ",
-                    "hour_microsecond ",
-                    "hour_minute     ",
-                    "hour_second ",
-                    "if ",
-                    "ignore ",
-                    "in ",
-                    "index ",
-                    "infile ",
-                    "inner ",
-                    "inout ",
-                    "insensitive ",
-                    "insert ",
-                    "int ",
-                    "integer ",
-                    "interval ",
-                    "into ",
-                    "is ",
-                    "iterate ",
-                    "join ",
-                    "key ",
-                    "keys ",
-                    "kill ",
-                    "leading ",
-                    "leave ",
-                    "left ",
-                    "like ",
-                    "limit ",
-                    "lines ",
-                    "load ",
-                    "localtime ",
-                    "localtimestamp ",
-                    "lock ",
-                    "long ",
-                    "longblob ",
-                    "longtext ",
-                    "loop ",
-                    "low_priority ",
-                    "match ",
-                    "mediumblob ",
-                    "mediumint ",
-                    "mediumtext ",
-                    "middleint ",
-                    "minute_microsecond ",
-                    "minute_second ",
-                    "mod ",
-                    "modifies ",
-                    "natural ",
-                    "not ",
-                    "no_write_to_binlog ",
-                    "null ",
-                    "numeric ",
-                    "on optimize ",
-                    "option ",
-                    "optionally ",
-                    "or ",
-                    "order ",
-                    "out ",
-                    "outer ",
-                    "outfile ",
-                    "precision ",
-                    "primary ",
-                    "procedure ",
-                    "purge ",
-                    "read ",
-                    "reads ",
-                    "real ",
-                    "references ",
-                    "regexp ",
-                    "rename ",
-                    "repeat ",
-                    "replace ",
-                    "require ",
-                    "restrict ",
-                    "return ",
-                    "revoke ",
-                    "right ",
-                    "rlike ",
-                    "schema ",
-                    "schemas ",
-                    "second_microsecond ",
-                    "select ",
-                    "sensitive ",
-                    "separator ",
-                    "set ",
-                    "show ",
-                    "smallint ",
-                    "soname ",
-                    "spatia ",
-                    "specific ",
-                    "sql ",
-                    "sqlexception ",
-                    "sqlstate ",
-                    "sqlwarning ",
-                    "sql_big_result ",
-                    "sql_calc_found_rows ",
-                    "sql_small_result ",
-                    "ssl ",
-                    "starting ",
-                    "straight_join ",
-                    "table ",
-                    "terminated ",
-                    "then ",
-                    "tinyblob ",
-                    "tinyint ",
-                    "tinytext ",
-                    "to ",
-                    "trailing ",
-                    "trigger ",
-                    "true ",
-                    "undo ",
-                    "union ",
-                    "unique ",
-                    "unlock ",
-                    "unsigned ",
-                    "update ",
-                    "usage ",
-                    "use ",
-                    "using ",
-                    "utc_date ",
-                    "utc_time ",
-                    "utc_timestamp ",
-                    "values ",
-                    "varbinary ",
-                    "varchar ",
-                    "varcharacter ",
-                    "varying ",
-                    "when ",
-                    "where ",
-                    "while ",
-                    "with ",
-                    "write ",
-                    "xor ",
-                    "year_month ",
-                    "zerofil "
-                ];
+            var reserved = [
+                "add ",
+                "all ",
+                "alter ",
+                "analyze ",
+                "and ",
+                "as ",
+                "asc ",
+                "asensitive ",
+                "before ",
+                "between ",
+                "bigint ",
+                "binary ",
+                "blob ",
+                "both ",
+                "by ",
+                "call ",
+                "cascade ",
+                "case ",
+                "change ",
+                "char ",
+                "character ",
+                "check ",
+                "collate ",
+                "column ",
+                "condition ",
+                "connection ",
+                "constraint ",
+                "continue ",
+                "convert ",
+                "create ",
+                "cross ",
+                "current_date ",
+                "current_time ",
+                "current_timestamp ",
+                "current_user ",
+                "cursor ",
+                "database ",
+                "databases ",
+                "day_hour ",
+                "day_microsecond ",
+                "day_minute ",
+                "day_second ",
+                "dec ",
+                "decimal ",
+                "declare ",
+                "default ",
+                "delayed ",
+                "delete ",
+                "desc ",
+                "describe ",
+                "deterministic ",
+                "distinct ",
+                "distinctrow ",
+                "div ",
+                "double ",
+                "drop ",
+                "dual ",
+                "each ",
+                "else ",
+                "elseif ",
+                "enclosed ",
+                "escaped ",
+                "exists ",
+                "exit ",
+                "explain ",
+                "false ",
+                "fetch ",
+                "float ",
+                "for ",
+                "force ",
+                "foreign ",
+                "from ",
+                "fulltext ",
+                "goto ",
+                "grant ",
+                "group ",
+                "having ",
+                "high_priority ",
+                "hour_microsecond ",
+                "hour_minute     ",
+                "hour_second ",
+                "if ",
+                "ignore ",
+                "in ",
+                "index ",
+                "infile ",
+                "inner ",
+                "inout ",
+                "insensitive ",
+                "insert ",
+                "int ",
+                "integer ",
+                "interval ",
+                "into ",
+                "is ",
+                "iterate ",
+                "join ",
+                "key ",
+                "keys ",
+                "kill ",
+                "leading ",
+                "leave ",
+                "left ",
+                "like ",
+                "limit ",
+                "lines ",
+                "load ",
+                "localtime ",
+                "localtimestamp ",
+                "lock ",
+                "long ",
+                "longblob ",
+                "longtext ",
+                "loop ",
+                "low_priority ",
+                "match ",
+                "mediumblob ",
+                "mediumint ",
+                "mediumtext ",
+                "middleint ",
+                "minute_microsecond ",
+                "minute_second ",
+                "mod ",
+                "modifies ",
+                "natural ",
+                "not ",
+                "no_write_to_binlog ",
+                "null ",
+                "numeric ",
+                "on optimize ",
+                "option ",
+                "optionally ",
+                "or ",
+                "order ",
+                "out ",
+                "outer ",
+                "outfile ",
+                "precision ",
+                "primary ",
+                "procedure ",
+                "purge ",
+                "read ",
+                "reads ",
+                "real ",
+                "references ",
+                "regexp ",
+                "rename ",
+                "repeat ",
+                "replace ",
+                "require ",
+                "restrict ",
+                "return ",
+                "revoke ",
+                "right ",
+                "rlike ",
+                "schema ",
+                "schemas ",
+                "second_microsecond ",
+                "select ",
+                "sensitive ",
+                "separator ",
+                "set ",
+                "show ",
+                "smallint ",
+                "soname ",
+                "spatia ",
+                "specific ",
+                "sql ",
+                "sqlexception ",
+                "sqlstate ",
+                "sqlwarning ",
+                "sql_big_result ",
+                "sql_calc_found_rows ",
+                "sql_small_result ",
+                "ssl ",
+                "starting ",
+                "straight_join ",
+                "table ",
+                "terminated ",
+                "then ",
+                "tinyblob ",
+                "tinyint ",
+                "tinytext ",
+                "to ",
+                "trailing ",
+                "trigger ",
+                "true ",
+                "undo ",
+                "union ",
+                "unique ",
+                "unlock ",
+                "unsigned ",
+                "update ",
+                "usage ",
+                "use ",
+                "using ",
+                "utc_date ",
+                "utc_time ",
+                "utc_timestamp ",
+                "values ",
+                "varbinary ",
+                "varchar ",
+                "varcharacter ",
+                "varying ",
+                "when ",
+                "where ",
+                "while ",
+                "with ",
+                "write ",
+                "xor ",
+                "year_month ",
+                "zerofil "
+            ];
 
-                var qtdReservedWord = reserved.length;
+            var qtdReservedWord = reserved.length;
 
-                for (let i = 0; i < qtdReservedWord; i++) {
-                    data = data.replace(reserved[i].toUpperCase(), "<span>"+reserved[i].toUpperCase()+"</span>");
-                    data = data.replace(reserved[i], "<span>"+reserved[i]+"</span>");
-                }
+            for (let i = 0; i < qtdReservedWord; i++) {
+                data = data.replace(reserved[i].toUpperCase(), "<span>"+reserved[i].toUpperCase()+"</span>");
+                data = data.replace(reserved[i], "<span>"+reserved[i]+"</span>");
+            }
 
-                return data;
-            };
+            return data;
+        };
 
-            let colored = replace(result.result);
+        let colored = replace(data);
 
-            this.setState({simpleResult: result.result});
-            this.setState({result: colored});
-            this.setState({load: null});
-
-        },)
+        this.setState({simpleResult: data});
+        this.setState({result: colored});
+        this.setState({load: null});
 
         e.preventDefault();
     }
@@ -320,6 +324,7 @@ class App extends React.Component {
     render() {
         return (
             <div className="App">
+                <div>Try our black theme in options ... </div>
                 <fieldset>
                     <legend className="legend"> Simple SQL</legend>
 
@@ -348,7 +353,8 @@ class App extends React.Component {
                     <button style={{display: this.state.result ? 'block' : 'none' }}
                             onClick={() => {navigator.clipboard.writeText(this.state.simpleResult)}}
                             className="buttonload">
-                        Copy
+                        <i className="fa fa-clipboard" aria-hidden="true" title={"copy to clipboard"}></i>
+
                     </button>
                 </fieldset>
             </div>
